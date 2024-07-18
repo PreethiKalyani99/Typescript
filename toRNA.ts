8b8f32d9adf85e91883f646b5b3618e435515456
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-07-18 16:08:47 +0530

    adds toRNA program

---
+++ toRNA.ts
@@ -0,0 +1,13 @@
+function toRNA(dna : string): string{
+    const DNA_to_RNA_map: {[index: string]:string} = {'G':'C', 'C':'G', 'T':'A', 'A':'U'}
+    const nucleotides = dna.split('')
+    let rna = ''
+    for(let i = 0; i < nucleotides.length; i++){
+        if(!DNA_to_RNA_map[nucleotides[i]]){
+            throw new Error("Invalid input DNA.")
+        }
+        rna += DNA_to_RNA_map[nucleotides[i]]
+    }
+    return rna
+}
+console.log(toRNA('TvA'))
