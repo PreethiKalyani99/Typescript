a5fa888da53e69764cb2117fefb448bec81c6396
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-07-22 12:58:30 +0530

    adds problem description

--- clock.ts
+++ clock.ts
@@ -1,3 +1,10 @@
+// Implement a clock that handles times without dates.
+
+// You should be able to add and subtract minutes to it.
+
+// Two clocks that represent the same time should be equal to each other.
+
+
 export class Clock  {
     private hour: number
     private minute: number
