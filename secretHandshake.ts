8c2c9b5e021867eca7ea5254f8a1821ffbde47de
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-07-24 20:07:50 +0530

    renames file name and function name

---
+++ secretHandshake.ts
@@ -0,0 +1,38 @@
+function reverseArray(arr: string[]): string[]{
+    let newArr: string[] = []
+    for(let i = arr.length - 1; i >= 0; i--){
+        newArr.push(arr[i])
+    }
+    return newArr
+}
+
+function secretHandshake(num: number): string[]{
+ let binaryValue: string = num.toString(2)
+ let arr : string[] = []
+
+    for(let i = binaryValue.length -  1; i >= 0; i--){
+        if(binaryValue[i] === '1'){
+            if(i === binaryValue.length - 1){
+                arr.push("wink")
+            }
+            if(i === binaryValue.length - 2){
+                arr.push("double blink")
+            }
+            if(i === binaryValue.length - 3){
+                arr.push("close your eyes")
+            }
+            if(i === binaryValue.length - 4){
+                arr.push("jump")
+            }
+            if(i === binaryValue.length - 5){
+                return reverseArray(arr)
+            }
+        }
+    }
+    return arr
+}
+
+console.log(secretHandshake(2)) 
+console.log(secretHandshake(0))
+console.log(secretHandshake(9))
+console.log(secretHandshake(15))
